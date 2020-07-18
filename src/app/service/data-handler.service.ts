975e57b3f860faac882c9666416afd07882b087b
David Dolgov <davidd7598@gmail.com>
2020-07-18 22:57:39 +0600

    добавили объекты Subject и подписчиков для отображения задач по категориям (все задачи пока не показываются)

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,11 +2,13 @@ import {Injectable} from '@angular/core';
 import {Category} from '../model/Category';
 import {TestData} from '../data/TestData';
 import {Task} from '../model/Task';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
+  taskSubject = new Subject<Task[]>();
 
   constructor() {
   }
@@ -15,11 +17,12 @@ export class DataHandlerService {
     return TestData.categories;
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
+  fillTasks() {
+    this.taskSubject.next(TestData.tasks);
   }
 
-  getTasksByCategory(category: Category): Task[] {
-    return TestData.tasks.filter(task => task.category === category);
+  fillTasksByCategory(category: Category) {
+    const tasks = TestData.tasks.filter(task => task.category === category);
+    this.taskSubject.next(tasks);
   }
 }
--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -14,7 +14,7 @@ export class TasksComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.tasks = this.dataHandler.getTasks();
+    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
   }
 
 }

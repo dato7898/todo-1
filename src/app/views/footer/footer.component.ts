6bbe20d216e671552fedc5fec524d71cf1d92fb4
David Dolgov <davidd7598@gmail.com>
2020-07-22 13:42:10 +0600

    Привели в порядок html. Разделили на компоненты

---
+++ src/app/views/footer/footer.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {AboutDialogComponent} from '../../dialog/about/about-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  year: Date;
+  site = 'https://javabegin.ru/';
+  blog = 'https://javabegin.ru/blog/tag/angular/';
+  siteName = 'JavaBegin';
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+    this.year = new Date(); // текуший год
+  }
+
+  // окно "О программе"
+  openAboutDialog() {
+    this.dialog.open(AboutDialogComponent,
+      {
+        autoFocus: false,
+        data: {
+          dialogTitle: 'О программе',
+          message: 'Данное приложение было создано для видеокурса "Angular для начинающих" на сайте javabegin.ru'
+        },
+        width: '400px'
+      });
+  }
+}

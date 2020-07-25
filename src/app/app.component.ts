fa0aadb2294e6c990429f61d168524309e4a9bbd
David Dolgov <davidd7598@gmail.com>
2020-07-25 23:36:35 +0600

    Добавил поиск категорий

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {concatMap, map} from 'rxjs/operators';
 import {IntroService} from './service/intro.service';
 import {DeviceDetectorService} from 'ngx-device-detector';
 import { CategoryService } from './data/dao/impl/CategoryService';
+import { CategoryTo } from './data/dao/to/ObjectsTo';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +22,9 @@ export class AppComponent implements OnInit {
 
   // статистика
   uncompletedCountForCategoryAll: number;
+
+  // параметры поисков
+  categoryTo = new CategoryTo(); // экземпляр можно создать тут же, т.к. не загружаем из cookies
 
   // показать/скрыть статистику
   showStat = true;
@@ -64,157 +68,41 @@ export class AppComponent implements OnInit {
     }
   }
 
+  addCategory(category: Category) {
+    this.categoryService.add(category).subscribe(result => {
+
+      }
+    );
+  }
+
+  deleteCategory(category: Category) {
+    this.categoryService.delete(category.id).subscribe(cat => {
+
+    });
+  }
+
+  updateCategory(category: Category) {
+    this.categoryService.update(category).subscribe(() => {
+
+    });
+  }
+
   // заполняет категории и кол-во невыполненных задач по каждой из них (нужно для отображения категорий)
   fillAllCategories() {
     this.categoryService.findAll().subscribe(result => {
       this.categories = result;
     });
-    // if (this.categoryMap) {
-    //   this.categoryMap.clear();
-    // }
-    // this.categories = this.categories.sort((a, b) => a.title.localeCompare(b.title));
-
-    // для каждой категории посчитать кол-во невыполненных задач
-    // this.categories.forEach(cat => {
-    //   this.dataHandler.getUncompletedCountInCategory(cat).subscribe(count => this.categoryMap.set(cat, count));
-    // });
-  }
-
-  selectCategory(category: Category) {
-    this.selectedCategory = category;
-    this.updateTasksAndStat();
-
-    if (this.isMobile) {
-      this.menuOpened = false; // закрываем боковое меню
-    }
-
-  }
-
-  updateCategory(category: Category) {
-    // this.dataHandler.updateCategory(category).subscribe(() => {
-    //   this.onSearchCategory(this.searchCategoryText);
-    // });
-  }
-
-  onUpdateTask(task: Task) {
-    // this.dataHandler.updateTask(task).subscribe(cat => {
-    //   this.fillCategories();
-    //   this.updateTasksAndStat();
-    // });
-  }
-
-  onDeleteTask(task: Task) {
-    // this.dataHandler.deleteTask(task.id).pipe(
-    //   concatMap(todo => {
-    //       return this.dataHandler.getUncompletedCountInCategory(todo.category).pipe(map(count => {
-    //         return ({t: todo, count});
-    //       }));
-    //     }
-    //   )).subscribe(result => {
-    //     const t = result.t as Task;
-    //     this.categoryMap.set(t.category, result.count);
-    //     this.updateTasksAndStat();
-    //   }
-    // );
-  }
-
-  deleteCategory(category: Category) {
-    // this.dataHandler.deleteCategory(category.id).subscribe(cat => {
-    //   this.selectedCategory = null; // открываем категорию "Все"
-    //   this.categoryMap.delete(cat); // не забыть удалить категорию из карты
-    //   this.onSearchCategory(this.searchCategoryText);
-    //   this.updateTasks();
-    // });
-  }
-
-  // поиск задач
-  onSearchTasks(searchString: string) {
-    // this.searchTaskText = searchString;
-    // this.updateTasks();
-  }
-
-  // фильтрация задач по статусу (все, решенные, нерешенные)
-  onFilterTasksByStatus(status: boolean) {
-    // this.statusFilter = status;
-    // this.updateTasks();
-  }
-
-  // фильрация задач по приоритету
-  onFilterTasksByPriority(priority: Priority) {
-    // this.priorityFilter = priority;
-    // this.updateTasks();
-  }
-
-  updateTasks() {
-    // this.dataHandler.searchTasks(
-    //   this.selectedCategory,
-    //   this.searchTaskText,
-    //   this.statusFilter,
-    //   this.priorityFilter
-    // ).subscribe((tasks: Task[]) => this.tasks = tasks);
-  }
-
-  // добавление задачи
-  onAddTask(task: Task) {
-    // this.dataHandler.addTask(task).pipe(// сначала добавляем задачу
-    //   concatMap(todo => { // используем добавленный task (concatMap - для последовательного выполнения)
-    //       // .. и считаем кол-во задач в категории с учетом добавленной задачи
-    //       return this.dataHandler.getUncompletedCountInCategory(todo.category).pipe(map(count => {
-    //         return ({t: todo, count}); // в итоге получаем массив с добавленной задачей и кол-вом задач для категории
-    //       }));
-    //     }
-    //   )).subscribe(result => {
-    //     const t = result.t as Task;
-    //
-    //     // если указана категория - обновляем счетчик для соотв. категории
-    //     if (t.category) {
-    //       this.categoryMap.set(t.category, result.count);
-    //     }
-    //     this.updateTasksAndStat();
-    //   }
-    // );
-  }
-
-  addCategory(title: string) {
-    // this.dataHandler.addCategory(title).subscribe(result => this.fillCategories());
   }
 
   // поиск категории
-  searchCategory(title: string) {
-    // this.searchCategoryText = title;
-    // this.dataHandler.searchCategories(title).subscribe(categories => {
-    //   this.categories = categories;
-    //   this.fillCategories();
-    // });
+  searchCategory(categoryTo: CategoryTo) {
+    this.categoryService.findCategories(categoryTo).subscribe(result => {
+      this.categories = result;
+    });
   }
 
-  // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
-  updateTasksAndStat() {
-    this.updateTasks(); // обновить список задач
+  selectCategory(category: Category) {
 
-    // обновить переменные для статистики
-    this.updateStat();
-  }
-
-  // обновить статистику
-  updateStat() {
-    // zip(
-    //   this.dataHandler.getTotalCountInCategory(this.selectedCategory),
-    //   this.dataHandler.getCompletedCountInCategory(this.selectedCategory),
-    //   this.dataHandler.getUncompletedCountInCategory(this.selectedCategory),
-    //   this.dataHandler.getUncompletedTotalCount())
-    //
-    //   .subscribe(array => {
-    //     this.totalTasksCountInCategory = array[0];
-    //     this.completedCountInCategory = array[1];
-    //     this.uncompletedCountInCategory = array[2];
-    //     this.uncompletedTotalTasksCount = array[3]; // нужно для категории Все
-    //   });
-  }
-
-  // показать-скрыть статистику
-  toggleStat(showStat: boolean) {
-    this.showStat = showStat;
   }
 
   // если закрыли меню любым способом - ставим значение false
--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -4,6 +4,7 @@ import {MatDialog} from '@angular/material/dialog';
 import {EditCategoryDialogComponent} from 'src/app/dialog/edit-category-dialog/edit-category-dialog.component';
 import {OperType} from 'src/app/dialog/OperType';
 import {DeviceDetectorService} from 'ngx-device-detector';
+import {CategoryTo} from '../../data/dao/to/ObjectsTo';
 
 @Component({
   selector: 'app-categories',
@@ -12,10 +13,13 @@ import {DeviceDetectorService} from 'ngx-device-detector';
 })
 export class CategoriesComponent implements OnInit {
   categories: Category[];
+
   @Input('categories')
   set setCategories(categories: Category[]) {
     this.categories = categories;
   }
+
+  @Input() categoryTo: CategoryTo;
   @Input() selectedCategory: Category;
   @Input() uncompletedCountForCategoryAll: number;
 
@@ -23,11 +27,13 @@ export class CategoriesComponent implements OnInit {
   @Output() deleteCategory = new EventEmitter<Category>();
   @Output() updateCategory = new EventEmitter<Category>();
   @Output() addCategory = new EventEmitter<string>();
-  @Output() searchCategory = new EventEmitter<string>(); // передаем строку для поиска
+  @Output() searchCategory = new EventEmitter<CategoryTo>(); // передаем строку для поиска
 
   // для отображения иконки редактирования при наведении на категорию
   indexMouseMove: number;
-  searchCategoryTitle: string; // текущее значение для поиска категорий
+  showEditIconCategory: boolean; // показывать ли иконку редактирования категории
+  filterTitle: string; // текущее значение для поиска категорий
+  filterChanged: boolean; // были ли изменения в параметре поиска
   isMobile: boolean;
   isTablet: boolean;
 
@@ -43,61 +49,47 @@ export class CategoriesComponent implements OnInit {
 
   }
 
-  showTasksByCategory(category: Category) {
-    if (this.selectedCategory === category) {
-      return;
-    }
-    this.selectedCategory = category;
+  // диалоговое окно для добавления категории
+  openAddDialog() {
 
-    // вызываем внешний обработчик и передаем туда выбранную категорию
-    this.selectCategory.emit(this.selectedCategory);
-  }
-
-  // сохраняет индекс записи категории, над который в данный момент проходит мышка (и там отображается иконка редактирования)
-  showEditIcon(index: number) {
-    this.indexMouseMove = index;
   }
 
   // диалоговое окно для редактирования категории
-  openEditCategoryDialog(category: Category) {
-    // открытие диалогового окна
-    const dialogRef = this.dialog.open(EditCategoryDialogComponent, {
-      data: [category.title, 'Редактирование категории', OperType.EDIT],
-      width: '400px'
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === 'delete') {
-        this.deleteCategory.emit(category);
-        return;
-      }
+  openEditDialog(category: Category) {
 
-      if (typeof (result) === 'string') { // если нажали ОК и есть результат
-        category.title = result as string;
-        this.updateCategory.emit(category);
-        return;
-      }
-    });
-  }
-
-  // диалоговое окно для добавления категории
-  openAddCategoryDialog() {
-    const dialogRef = this.dialog.open(EditCategoryDialogComponent, {
-      data: ['', 'Добавление категории', OperType.ADD],
-      width: '400px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.addCategory.emit(result as string); // вызываем внешний обработчик
-      }
-    });
   }
 
   // поиск категории
   search() {
-    if (this.searchCategoryTitle == null) {
+    this.filterChanged = false; // сбросить
+
+    if (!this.categoryTo) { // если объект с параметрами поиска непустой
       return;
     }
-    this.searchCategory.emit(this.searchCategoryTitle);
+
+    this.categoryTo.text = this.filterTitle;
+    this.searchCategory.emit(this.categoryTo);
   }
+
+  showCategory(category: Category) {
+
+  }
+
+  // сохраняет индекс записи категории, над который в данный момент проходит мышка (и там отображается иконка редактирования)
+  showEditIcon(show: boolean, index: number) {
+    this.indexMouseMove = index;
+    this.showEditIconCategory = show;
+  }
+
+  clearAndSearch() {
+    this.filterTitle = null;
+    this.search();
+  }
+
+  // проверяет, были ли изменены какие-либо параметры поиска (по сравнению со старым значением)
+  checkFilterChanged() {
+    this.filterChanged = this.filterTitle !== this.categoryTo.text;
+    return this.filterChanged;
+  }
+
 }
